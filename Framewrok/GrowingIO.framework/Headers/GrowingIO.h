cce5ddb9f53e5dd3aa936adbefc45065c0e1d235
liugao <liugao@riverolls.com>
2024-02-18 14:13:06 +0800

    init

---
+++ Framewrok/GrowingIO.framework/Headers/GrowingIO.h
@@ -0,0 +1,18 @@
+//
+//  GrowingIO.h
+//  GrowingIO
+//
+//  Created by fjdh on 2022/7/8.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GrowingIO.
+FOUNDATION_EXPORT double GrowingIOVersionNumber;
+
+//! Project version string for GrowingIO.
+FOUNDATION_EXPORT const unsigned char GrowingIOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GrowingIO/PublicHeader.h>
+
+
